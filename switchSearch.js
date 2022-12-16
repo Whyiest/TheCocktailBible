ab4a8bd93b66f41042fe3d7f18971d035722fb23
Whyiest <74537743+Whyiest@users.noreply.github.com>
2022-12-15 21:54:37 -0500

    Adding effect on search page, adding placeholder for message and cocktail, reformat document

--- switchSearch.js
+++ switchSearch.js
@@ -2,42 +2,44 @@ let switchButton = document.getElementById("switcher-container");
 let searchText = document.getElementById("switcher-text");
 let searchBarText = document.getElementById("search-bar");
 let searchButton = document.getElementById("search-button");
+let resultMessageArea = document.getElementById("result-message");
+// Variable stored know what we are looking for
+sessionStorage.setItem('pageState', 'name');
 
-let searchState = new Array(1);
-searchState[0] = 1;
-// 0 = Currently searching by ingredient
-// 1 = Currently searching by name
 
 function processRequest () {
+
     const urlParams = new URLSearchParams(window.location.search);
     const searchValue = urlParams.get('s');
+
     if (searchValue == "ingredient") {
-        switchSearch(searchState);
+        switchSearch();
     }
 
 }
 
-function switchSearch (searchState) {
+function switchSearch () {
 
-    if (searchState[0] == 0) {
+    if (sessionStorage.getItem('pageState') === 'ingredient') {
         resultGrid.innerHTML = "";
         searchText.innerHTML = "Search by <span class=\"switch-state-color\">ingredients</span>";
         searchBarText.placeholder = "Search a cocktail by name";
         searchButton.setAttribute('onclick','getCocktailsByName()')
-
-        searchState[0] = 1;
+        sessionStorage.setItem('pageState', 'name');
+        resultMessageArea.innerHTML = "Search a cocktail with the field above !";
     }
-    else if (searchState[0] == 1) {
+    else if (sessionStorage.getItem('pageState') === 'name') {
+
         resultGrid.innerHTML = "";
         searchText.innerHTML = "Search by <span class=\"switch-state-color\">name</span>";
-        searchBarText.placeholder = "Search a cocktail with specified ingredients";
+        searchBarText.placeholder = "Search a cocktail by ingredients";
         searchButton.setAttribute('onclick','getCocktailsByIngredient()')
-
-        searchState[0] = 0;
+        sessionStorage.setItem('pageState', 'ingredient');
     }
     else {
         alert("Critical error, refreshing page...");
-        window.location.reload();
+        //window.location.reload();
     }
     return false
 }
+
