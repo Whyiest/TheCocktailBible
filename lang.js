cfe07b3fa021e5fb5990eda745ccd2569f6af31b
dolig <thomas.dolige@edu.ece.fr>
2022-12-16 03:59:05 +0100

    adding lang

--- lang.js
+++ lang.js
@@ -1,6 +1,8 @@
 let home=false;
 let search=false;
 let random=false;
+let lang;
+
 
 
 
@@ -14,7 +16,108 @@ function searchActive(){
 
 function randomActive(){
     random=true;
+
 }
+
+function translateHomeFr(){
+    document.getElementById("nav-list").innerHTML= "<li class=\"nav-element\"><a class=\"nav-link\" href=\"index.html\"><i class=\"fa-solid fa-house-chimney\"></i>&nbsp;Accueil</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html\">Nom</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html?s=ingredient\">Ingredients</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"random.html\">Aléatoire</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"mailto:contact@cocktail.bible.com\">Contactez-nous</a></li>"
+    document.getElementById("home-text").innerHTML="La Bible du <span class=\"colored-text\">Cocktail</span>";
+    document.getElementById("home-button-byname-text").innerHTML="Chercher par nom&nbsp; <i class=\"fa-solid fa-spell-check\"></i>";
+    document.getElementById("home-button-byingredient-text").innerHTML="Chercher par ingrédient&nbsp; <i class=\"fa-solid fa-lemon\"></i>";
+    document.getElementById("home-button-random-text").innerHTML="Cocktail au hasard ?&nbsp; <i class=\"fa-solid fa-dice\"></i>";
+    localStorage.setItem('lang', 'fr')
+
+}
+
+function translateHomeEn(){
+    document.getElementById("nav-list").innerHTML= "<li class=\"nav-element\"><a class=\"nav-link\" href=\"index.html\"><i class=\"fa-solid fa-house-chimney\"></i>&nbsp;Home</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html\">Name</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html?s=ingredient\">Ingredients</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"random.html\">Random</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"mailto:contact@cocktail.bible.com\">Contact</a></li>"
+    document.getElementById("home-text").innerHTML="The <span class=\"colored-text\">Cocktail</span> Bible";
+    document.getElementById("home-button-byname-text").innerHTML="Search by name&nbsp; <i class=\"fa-solid fa-spell-check\"></i>";
+    document.getElementById("home-button-byingredient-text").innerHTML="Search by ingredient&nbsp; <i class=\"fa-solid fa-lemon\"></i>";
+    document.getElementById("home-button-random-text").innerHTML="Random cocktail ?&nbsp; <i class=\"fa-solid fa-dice\"></i>";
+    localStorage.setItem('lang', 'en')
+}
+
+function translateRandomEn(){
+    document.getElementById("nav-list").innerHTML= "<li class=\"nav-element\"><a class=\"nav-link\" href=\"index.html\"><i class=\"fa-solid fa-house-chimney\"></i>&nbsp;Home</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html\">Name</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html?s=ingredient\">Ingredients</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"random.html\">Random</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"mailto:contact@cocktail.bible.com\">Contact</a></li>"
+    document.getElementById("random").innerHTML=" <p id=\"random-text\">Find <span class=\"colored-text\">Random</span> cocktail</p><p id=\"random-message\">Search a cocktail with the field above !</p><div id=\"random-container\"onclick=\"getRandomCocktails()\"><div id=\"random-empty-result\"></div></div>"
+    localStorage.setItem('lang', 'en')
+}
+
+function translateRandomFr(){
+    document.getElementById("nav-list").innerHTML= "<li class=\"nav-element\"><a class=\"nav-link\" href=\"index.html\"><i class=\"fa-solid fa-house-chimney\"></i>&nbsp;Accueil</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html\">Nom</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html?s=ingredient\">Ingredients</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"random.html\">Aléatoire</a></li>"
+        +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"mailto:contact@cocktail.bible.com\">Contactez-nous</a></li>"
+
+
+    document.getElementById("random").innerHTML=" <p id=\"random-text\">Trouver <span class=\"colored-text\">au hasard</span> un cocktail</p><p id=\"random-message\">Cherchez un cocktail avec le champ ci-dessus !</p><div id=\"random-container\"onclick=\"getRandomCocktails()\"><div id=\"random-empty-result\"></div></div>"
+    localStorage.setItem('lang', 'fr')
+}
+
+function translateSearchEn() {
+    document.getElementById("banner-container").innerHTML="<div id=\"switcher-container\" onclick=\"switchSearch(searchState)\"><p id=\"switcher-text\">Search by <span class=\"button-text-color\">name</span></p><img id=\"switcher-icon\" src=\"images/switch-icon-white.png\" alt=\"switch arrow\"></div><p id=\"banner-title\">Search</p><div id=\"back-button\"><a id=\"back-button-text\" href=\"index.html\"> <i class=\"fa-solid fa-circle-chevron-left\"></i>&nbsp;&nbsp;Go back to <span class=\"button-text-color\">menu</span></a><!--<img id=\"back-button-icon\" src=\"images/left-arrow-black.svg\" alt=\"back arrowv\">--></div>"
+    document.getElementById("search-bar").setAttribute("placeholder","Search a cocktail by name")
+    document.getElementById("result-message").innerHTML="Loading search API..."
+    localStorage.setItem('lang', 'en')
+}
+
+function translateSearchFr(){
+    document.getElementById("banner-container").innerHTML="<div id=\"switcher-container\" onclick=\"switchSearch(searchState)\"><p id=\"switcher-text\">Chercher par <span class=\"button-text-color\">nom</span></p><img id=\"switcher-icon\" src=\"images/switch-icon-white.png\" alt=\"switch arrow\"></div><p id=\"banner-title\">Rechercher</p><div id=\"back-button\"><a id=\"back-button-text\" href=\"index.html\"> <i class=\"fa-solid fa-circle-chevron-left\"></i>&nbsp;&nbsp;Retourner au <span class=\"button-text-color\">menu</span></a><!--<img id=\"back-button-icon\" src=\"images/left-arrow-black.svg\" alt=\"back arrowv\">--></div>"
+    document.getElementById("search-bar").setAttribute("placeholder","Rechercher un cocktail par nom")
+    document.getElementById("result-message").innerHTML="Chargement de l'API..."
+    localStorage.setItem('lang', 'fr')
+
+}
+
+function checkLanguageHome(){
+    if(localStorage.getItem('lang')==='fr'){
+        document.getElementById("language-select").innerHTML="<option value=\"fr\">FR</option><option value=\"en\">EN</option>"
+        translateHomeFr()
+    }
+    else if(localStorage.getItem('lang')==='en'){
+        translateHomeEn()
+    }
+
+}
+
+function checkLanguageRandom(){
+    if(localStorage.getItem('lang')==='fr'){
+        document.getElementById("language-select").innerHTML="<option value=\"fr\">FR</option><option value=\"en\">EN</option>"
+        translateRandomFr()
+    }
+    else if(localStorage.getItem('lang')==='en'){
+        translateRandomEn()
+    }
+
+}
+
+
+function checkLanguageSearch(){
+    if(localStorage.getItem('lang')==='fr'){
+        translateSearchFr()
+    }
+    else if(localStorage.getItem('lang')==='en'){
+        translateSearchEn()
+    }
+}
+
+
+
+
 
 
 
@@ -27,33 +130,11 @@ $("#language-select").change(function() {
 
 
         if (language === "en") {
-            document.getElementById("nav-list").innerHTML= "<li class=\"nav-element\"><a class=\"nav-link\" href=\"index.html\"><i class=\"fa-solid fa-house-chimney\"></i>&nbsp;Home</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html\">Name</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html?s=ingredient\">Ingredients</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"random.html\">Random</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"mailto:contact@cocktail.bible.com\">Contact</a></li>"
-            document.getElementById("home-text").innerHTML="The <span class=\"colored-text\">Cocktail</span> Bible";
-            document.getElementById("home-button-byname-text").innerHTML="Search by name&nbsp; <i class=\"fa-solid fa-spell-check\"></i>";
-            document.getElementById("home-button-byingredient-text").innerHTML="Search by ingredient&nbsp; <i class=\"fa-solid fa-lemon\"></i>";
-            document.getElementById("home-button-random-text").innerHTML="Random cocktail ?&nbsp; <i class=\"fa-solid fa-dice\"></i>";
-
+            translateHomeEn()
 
         } else if (language === "fr") {
 
-
-            document.getElementById("nav-list").innerHTML= "<li class=\"nav-element\"><a class=\"nav-link\" href=\"index.html\"><i class=\"fa-solid fa-house-chimney\"></i>&nbsp;Accueil</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html\">Nom</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html?s=ingredient\">Ingredients</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"random.html\">Aléatoire</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"mailto:contact@cocktail.bible.com\">Contactez-nous</a></li>"
-            document.getElementById("home-text").innerHTML="La Bible du <span class=\"colored-text\">Cocktail</span>";
-            document.getElementById("home-button-byname-text").innerHTML="Chercher par nom&nbsp; <i class=\"fa-solid fa-spell-check\"></i>";
-            document.getElementById("home-button-byingredient-text").innerHTML="Chercher par ingrédient&nbsp; <i class=\"fa-solid fa-lemon\"></i>";
-            document.getElementById("home-button-random-text").innerHTML="Cocktail au hasard ?&nbsp; <i class=\"fa-solid fa-dice\"></i>";
-
-
-
-
+           translateHomeFr()
         }}
 
     if(random){
@@ -63,30 +144,11 @@ $("#language-select").change(function() {
 
         if (language === "en") {
 
+            translateRandomEn()
 
-            document.getElementById("nav-list").innerHTML= "<li class=\"nav-element\"><a class=\"nav-link\" href=\"index.html\"><i class=\"fa-solid fa-house-chimney\"></i>&nbsp;Home</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html\">Name</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html?s=ingredient\">Ingredients</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"random.html\">Random</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"mailto:contact@cocktail.bible.com\">Contact</a></li>"
-            document.getElementById("random").innerHTML=" <p id=\"random-text\">Find <span class=\"colored-text\">Random</span> cocktail</p><p id=\"random-message\">Search a cocktail with the field above !</p><div id=\"random-container\"onclick=\"getRandomCocktails()\"><div id=\"random-empty-result\"></div></div>"
+        } else if (language === "fr") {
 
-
-
-
-
-        } else if (language === "fr" ) {
-
-            document.getElementById("nav-list").innerHTML= "<li class=\"nav-element\"><a class=\"nav-link\" href=\"index.html\"><i class=\"fa-solid fa-house-chimney\"></i>&nbsp;Accueil</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html\">Nom</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"search.html?s=ingredient\">Ingredients</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"random.html\">Aléatoire</a></li>"
-                +"<li class=\"nav-element\"><a class=\"nav-link\" href=\"mailto:contact@cocktail.bible.com\">Contactez-nous</a></li>"
-
-            document.getElementById("random").innerHTML=" <p id=\"random-text\">Trouver <span class=\"colored-text\">au hasard</span> un cocktail</p><p id=\"random-message\">Cherchez un cocktail avec le champ ci-dessus !</p><div id=\"random-container\"onclick=\"getRandomCocktails()\"><div id=\"random-empty-result\"></div></div>"
-
-
-
+            translateRandomFr()
 
         }
 
@@ -101,28 +163,13 @@ $("#language-select").change(function() {
 
         if (language === "en") {
 
-
-            document.getElementById("banner-container").innerHTML="<div id=\"switcher-container\" onclick=\"switchSearch(searchState)\"><p id=\"switcher-text\">Search by <span class=\"button-text-color\">name</span></p><img id=\"switcher-icon\" src=\"images/switch-icon-white.png\" alt=\"switch arrow\"></div><p id=\"banner-title\">Search</p><div id=\"back-button\"><a id=\"back-button-text\" href=\"index.html\"> <i class=\"fa-solid fa-circle-chevron-left\"></i>&nbsp;&nbsp;Go back to <span class=\"button-text-color\">menu</span></a><!--<img id=\"back-button-icon\" src=\"images/left-arrow-black.svg\" alt=\"back arrowv\">--></div>"
-            document.getElementById("search-bar").setAttribute("placeholder","Search a cocktail by name")
-            document.getElementById("result-message").innerHTML="Loading search API..."
-
+            translateSearchEn()
 
 
         } else if (language === "fr" ) {
 
-
-
-            document.getElementById("banner-container").innerHTML="<div id=\"switcher-container\" onclick=\"switchSearch(searchState)\"><p id=\"switcher-text\">Chercher par <span class=\"button-text-color\">nom</span></p><img id=\"switcher-icon\" src=\"images/switch-icon-white.png\" alt=\"switch arrow\"></div><p id=\"banner-title\">Rechercher</p><div id=\"back-button\"><a id=\"back-button-text\" href=\"index.html\"> <i class=\"fa-solid fa-circle-chevron-left\"></i>&nbsp;&nbsp;Retourner au <span class=\"button-text-color\">menu</span></a><!--<img id=\"back-button-icon\" src=\"images/left-arrow-black.svg\" alt=\"back arrowv\">--></div>"
-            document.getElementById("search-bar").setAttribute("placeholder","Rechercher un cocktail par nom")
-            document.getElementById("result-message").innerHTML="Chargement de l'API..."
-
-
+            translateSearchFr()
 
         }
-
-
     }
-
-
-
 });
